c97bb6ed5ecc35577c0d9da28ffb82c7d7b76796
Aichi Chang <phoebexxxc@gmail.com>
2020-01-28 08:58:25 +0000

    Ready for deployment

---
+++ aichi-chang/build/precache-manifest.85a7c98397c48d3e67ff76d052548bb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb4c10380b600bcef768959cb134cf0f",
+    "url": "/aichi/index.html"
+  },
+  {
+    "revision": "98046d3910a7a6be2f5a",
+    "url": "/aichi/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8022a7918a83389fba47",
+    "url": "/aichi/static/css/main.3862575c.chunk.css"
+  },
+  {
+    "revision": "98046d3910a7a6be2f5a",
+    "url": "/aichi/static/js/2.06232324.chunk.js"
+  },
+  {
+    "revision": "a1ddc28aa94628a9d685d0c81c91c39c",
+    "url": "/aichi/static/js/2.06232324.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8022a7918a83389fba47",
+    "url": "/aichi/static/js/main.457b7e26.chunk.js"
+  },
+  {
+    "revision": "0d20c1bb3f96578e4f93",
+    "url": "/aichi/static/js/runtime-main.5912730b.js"
+  }
+]);
