cc51e6295e3e11ec0a8b2506b9565914b7a0c306
Aichi Chang <phoebexxxc@gmail.com>
2020-02-06 12:57:29 +0000

    final styling

--- aichi-chang/src/App.js
+++ aichi-chang/src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { HashRouter, Switch, Route, BrowserRouter } from 'react-router-dom'
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
 import './App.css';
 import 'tachyons'
 
-// import Projects from './components/Projects'
 import TitleSequence from './components/TitleSequence'
-// import Information from './components/Information'
-// import Contact from './components/Contact'
-import Navs from './components/Navs'
 import Portfolio from './components/Portfolio'
 
 
@@ -15,27 +11,7 @@ import Portfolio from './components/Portfolio'
 function App() {
 
   return (
-    // <div>
-      
-    //   <HashRouter>
-        
-    //       <TitleSequence />
-    //       <Projects />
-    //       <Information />
-    //       <Contact />
-        
 
-    //   <div className='navs'>
-    //     <Navs />
-    //   </div>
-
-    //     {/* <Switch>
-    //     <Route exact path = '/projects' component={Projects} />
-    //     <Route exact path = '/information' component={Information} />
-    //     </Switch> */}
-
-    //   </HashRouter>
-    // </div>
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={TitleSequence} />
