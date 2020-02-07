39f6ee4b319338ccb0d43f03973388123eb99dff
Aichi Chang <phoebexxxc@gmail.com>
2020-02-07 23:59:56 +0000

    Ready for deployment

--- aichi-chang/src/components/Navs.js
+++ aichi-chang/src/components/Navs.js
@@ -1,20 +1,19 @@
 import React from 'react'
-import {NavHashLink as NavLink} from 'react-router-hash-link'
 
 
 export default function Navs(props) {
   // console.log(props.location.hash)
   return (
     <div className='flex flex-column-l'>
-      < NavLink to='#projects'  className={`ba br2 pt1 pb1 pr4-l pl4-l pr3 pl3 mb2-l ml1 no-underline near-black pointer navs-link tc ${props.location.hash === '#projects' && 'silver'}`}>
+      < a href='#projects'  className={`ba br2 pt1 pb1 pr4-l pl4-l pr3 pl3 mb2-l ml1 no-underline near-black pointer navs-link tc ${props.location.hash === '#projects' && 'silver'}`}>
         Projects
-      </ NavLink>
-      < NavLink to='#information' exact className={`ba br2 pt1 pb1 pr4-l pl4-l pr3 pl3 mb2-l ml1 no-underline near-black pointer navs-link tc ${props.location.hash === '#information' && 'silver'}`}>
+      </ a>
+      < a href='#information' exact className={`ba br2 pt1 pb1 pr4-l pl4-l pr3 pl3 mb2-l ml1 no-underline near-black pointer navs-link tc ${props.location.hash === '#information' && 'silver'}`}>
         Information
-      </ NavLink>
-      < NavLink to='#contact' exact className={`ba br2 pt1 pb1 pr4-l pl4-l pr3 pl3 mb2-l ml1 no-underline near-black pointer navs-link tc ${props.location.hash === '#contact' && 'silver'}`}>
+      </ a>
+      < a href='#contact' exact className={`ba br2 pt1 pb1 pr4-l pl4-l pr3 pl3 mb2-l ml1 no-underline near-black pointer navs-link tc ${props.location.hash === '#contact' && 'silver'}`}>
         Contact
-      </ NavLink>
+      </ a>
     </div>
 
   )
