350dda7cb56ec77a95304264b2bbd2aa11e9ce98
Aichi Chang <phoebexxxc@gmail.com>
2020-02-16 21:12:37 +0000

    canvas

--- aichi-chang/src/App.js
+++ aichi-chang/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import { Switch, Route, HashRouter } from 'react-router-dom'
 import './App.css';
 import 'tachyons'
 
@@ -12,14 +12,14 @@ function App() {
 
   return (
 
-    <BrowserRouter>
+    <HashRouter>
 
       <Switch>
         <Route exact path="/" component={TitleSequence} />
         <Route exact path='/portfolio' component={Portfolio} />
 
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
--- aichi-chang/src/components/Navs.js
+++ aichi-chang/src/components/Navs.js
@@ -1,19 +1,19 @@
 import React from 'react'
-
+import { HashLink as Link } from 'react-router-hash-link'
 
 export default function Navs(props) {
   // console.log(props.location.hash)
   return (
     <div className='flex flex-column-l'>
-      < a href='#projects'  className={`ba br2 pt1 pb1 pr4-l pl4-l pr3 pl3 mb2-l ml1 no-underline near-black pointer navs-link tc ${props.location.hash === '#projects' && 'silver'}`}>
+      < Link to='/portfolio#projects'  className={`ba br2 pt1 pb1 pr4-l pl4-l pr3 pl3 mb2-l ml1 no-underline near-black pointer navs-link tc ${props.location.hash === '#projects' && 'silver'}`}>
         Projects
-      </ a>
-      < a href='#information' exact className={`ba br2 pt1 pb1 pr4-l pl4-l pr3 pl3 mb2-l ml1 no-underline near-black pointer navs-link tc ${props.location.hash === '#information' && 'silver'}`}>
+      </ Link>
+      < Link to='/portfolio#information' exact className={`ba br2 pt1 pb1 pr4-l pl4-l pr3 pl3 mb2-l ml1 no-underline near-black pointer navs-link tc ${props.location.hash === '#information' && 'silver'}`}>
         Information
-      </ a>
-      < a href='#contact' exact className={`ba br2 pt1 pb1 pr4-l pl4-l pr3 pl3 mb2-l ml1 no-underline near-black pointer navs-link tc ${props.location.hash === '#contact' && 'silver'}`}>
+      </ Link>
+      < Link to='/portfolio#contact' exact className={`ba br2 pt1 pb1 pr4-l pl4-l pr3 pl3 mb2-l ml1 no-underline near-black pointer navs-link tc ${props.location.hash === '#contact' && 'silver'}`}>
         Contact
-      </ a>
+      </ Link>
     </div>
 
   )
